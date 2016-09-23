24162a16b0d459df50c4a3392a6291d8368bcbd4
xianyinyin05181986 <xianyinyin05181986@gmail.com>
2016-09-23 16:17:02 +1000

    1st version

---
+++ EmailConsoleApplication/EmailConsoleApplication/EmailValidater.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EmailConsoleApplication
+{
+    public class EmailValidater
+    {
+        public const string STR_VALID = "Valid";
+        public const string STR_INVALID = "Invalid";
+
+        public string EmailAddress { get; set; }
+        public EmailValidater() { }
+        public EmailValidater(string emailAddress) { EmailAddress = emailAddress; }
+
+        public bool ValidateEmailAddressUsingRegularExpress(string emailAddress)
+        {
+            var r = new Regex(@"^([0-9a-zA-Z]([-\.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$");
+
+            return !string.IsNullOrEmpty(emailAddress) && r.IsMatch(emailAddress);
+        }
+
+        public bool ValidateEmailAddressUsingRegularExpress_Alternative(string emailAddress)
+        {
+            var r = new Regex(@"^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$",
+                  RegexOptions.IgnoreCase);
+
+            return !string.IsNullOrEmpty(emailAddress) && r.IsMatch(emailAddress);
+        }
+
+        public bool IsEmailAddressValid(string emailAddress)
+        {
+            return ValidateEmailAddressUsingRegularExpress(emailAddress);
+        }
+
+        public string String_IsEmailAddressValid(bool emailAddress)
+        {
+            if (emailAddress)
+            {
+                return STR_VALID;
+            }
+            else
+            {
+                return STR_INVALID;
+            }
+        }
+
+        public string String_IsEmailAddressValid(string emailAddress)
+        {
+            if (IsEmailAddressValid(emailAddress))
+            {
+                return String.Format("Result: {0} for Email {1}",STR_VALID,emailAddress);
+            }
+            else
+            {
+                return String.Format("Result: {0} for Email {1}", STR_INVALID, emailAddress);
+            }
+        }
+    }
+}
---
+++ EmailConsoleApplication/EmailConsoleApplication/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmailConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var result = new EmailValidater();
+
+
+            var list = new List<string>();
+
+            #region Valid
+            list.Add(String.Format("email@domain.com"));
+            list.Add(String.Format("firstname.lastname@domain.com"));
+            list.Add(String.Format("email@subdomain.domain.com"));
+            list.Add(String.Format("firstname+lastname@domain.com"));
+            list.Add(String.Format("email@123.123.123.123"));
+            list.Add(String.Format("email@[123.123.123.123]"));
+            list.Add(String.Format("“email”@domain.com"));
+            list.Add(String.Format("1234567890@domain.com"));
+            list.Add(String.Format("email@domain-one.com"));
+            list.Add(String.Format("_______@domain.com"));
+            list.Add(String.Format("email@domain.name"));
+            list.Add(String.Format("email@domain.co.jp"));
+            list.Add(String.Format("firstname-lastname@domain.com")); 
+            #endregion
+
+
+            //list.Add(String.Format(""));
+            //list.Add(String.Format(""));
+            //list.Add(String.Format(""));
+
+
+            foreach (var email in list)
+            {
+                Console.WriteLine(result.String_IsEmailAddressValid(email));
+            }
+            Console.ReadLine();
+        }
+    }
+}
